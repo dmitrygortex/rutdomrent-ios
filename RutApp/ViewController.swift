0c7dff2fa61883f9e7e25638d4cecb8271ab22c5
Michael <michaelkivo977@gmail.com>
2024-01-14 20:53:38 +0300

    Create empty project

---
+++ RutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RutApp
+//
+//  Created by Michael Kivo on 14/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
