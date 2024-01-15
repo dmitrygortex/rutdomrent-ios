cb47ac1c52752b117ef0c805602bcadb1d2716e1
Michael <michaelkivo977@gmail.com>
2024-01-15 20:24:46 +0300

    Add Info, Booking and AccountVC's

---
+++ RutApp/ViewControllers/AccountViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AccountViewController.swift
+//  RutApp
+//
+//  Created by Michael Kivo on 15/01/2024.
+//
+
+import UIKit
+import SnapKit
+
+class AccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ RutApp/ViewControllers/InfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  InfoViewController.swift
+//  RutApp
+//
+//  Created by Michael Kivo on 15/01/2024.
+//
+
+import UIKit
+import SnapKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
