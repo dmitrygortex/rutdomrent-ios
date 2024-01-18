2e2f07a58d4924e842c04de1825ada5e74fac249
Michael <michaelkivo977@gmail.com>
2024-01-18 22:51:33 +0300

    Add ScheduleVC

---
+++ RutApp/ViewControllers/ScheduleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleViewController.swift
+//  RutApp
+//
+//  Created by Michael Kivo on 18/01/2024.
+//
+
+import UIKit
+import SnapKit
+import Firebase
+
+class ScheduleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+}
